c7e886b7362326321a638ff2a1e34dc5aa4e8353
toshirohex <toshibahex@gmail.com>
2021-03-07 13:51:57 -0500

    did more stuff
    idrk what to say at this point but ye there was more stuff added.  did more work on armor, should hopefully have beta 0.0.2 ready by tonight

--- src/main/java/net/toshirohex/lycurgus/materials/ModArmorMaterials.java
+++ src/main/java/net/toshirohex/lycurgus/materials/ModArmorMaterials.java
@@ -4,31 +4,19 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ArmorMaterials;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Lazy;
 import net.toshirohex.lycurgus.registry.ModItems;
 
 import java.util.function.Supplier;
 
 public enum ModArmorMaterials implements ArmorMaterial {
 
-    STEEL("steel", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> {
-        return Ingredient.ofItems(ModItems.Ingots[0]);
-    }),
-    HANDS_COLD("hands_cold", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_TURTLE, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(ModItems.Ingots[1]);
-    }),
-    ENDIUM("endium", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> {
-        return Ingredient.ofItems(ModItems.Ingots[2]);
-    }),
-    KNIGHT("knight", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_TURTLE, 0.0F, 0.0F, () -> {
-        return null;
-    });
+    STEEL("steel", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 2.0F, 0.0F, () -> Ingredient.ofItems(ModItems.Ingots[0])),
+    HANDS_COLD("hands_cold", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> Ingredient.ofItems(ModItems.Ingots[1])),
+    ENDIUM("endium", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 3.0F, 0.1F, () -> Ingredient.ofItems(ModItems.Ingots[2])),
+    KNIGHT("knight", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> null);
 
     private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
     private final String name;
@@ -51,35 +39,43 @@ public enum ModArmorMaterials implements ArmorMaterial {
         this.repairIngredientSupplier = repairIngredientSupplier;
     }
 
+    @Override
     public int getDurability(EquipmentSlot slot) {
         return BASE_DURABILITY[slot.getEntitySlotId()] * durabilityMultiplier;
     }
 
+    @Override
     public int getProtectionAmount(EquipmentSlot slot) {
-        return this.protectionAmounts[slot.getEntitySlotId()];
+        return protectionAmounts[slot.getEntitySlotId()];
     }
 
+    @Override
     public int getEnchantability() {
         return enchantability;
     }
 
+    @Override
     public SoundEvent getEquipSound() {
         return equipSound;
     }
 
+    @Override
     public Ingredient getRepairIngredient() {
         return repairIngredientSupplier.get();
     }
 
+    @Override
     @Environment(EnvType.CLIENT)
     public String getName() {
         return name;
     }
 
+    @Override
     public float getToughness() {
         return toughness;
     }
 
+    @Override
     public float getKnockbackResistance() {
         return knockbackResistance;
     }
